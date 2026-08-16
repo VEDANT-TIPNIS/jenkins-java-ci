6835c1350af37fe74e43be8339910d9e9d3d95aa
Vedant Tipnis <vedant.tipnis@somaiya.edu>
2026-08-16 12:35:51 +0530

    Test GitHub webhook

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins CI!");
         System.out.println("Updated output message!");
+	System.out.println("Webhook test from GitHub!");
     }
 }
